5627a4d2c0c7f48797eb502df22a5aed880a396e
ArturVoroshilov <ipvoroshilov@ya.ru>
2022-08-28 21:40:35 +0300

    Completed.Task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// ## Задача 23
+
+// ### Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string function()
+ {
+    Console.Write("Imput number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int count=1;
+            string result = string.Empty;   //пустая строка
+            while (count <= N)
+            {
+                result = result + Math.Pow(count, 3)+", ";
+                count=count+1;             
+            }
+    return result;
+ }
+ try
+ {
+    Console.WriteLine(function());
+ }
+catch
+{
+    Console.WriteLine("Not number");
+}
